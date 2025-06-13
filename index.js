2804febe0fa85bf3bb0646721f2848a274a8c6a9
+ <+githinjiadreen27@gmail.com>
2025-06-13 21:59:36 +0300

    Scope

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Write your solution in this file!
+var customerName = 'bob';
+
+var bestCustomer;
+
+const leastFavouriteCustomer = 'Mike';
+
+
+function upperCaseCustomerName(){
+    customerName = customerName.toUpperCase();
+}
+
+function setBestCustomer(){
+    bestCustomer = 'not bob';
+}
+
+function overwriteBestCustomer(){
+    bestCustomer = 'maybe bob';
+}
+
+
+function changeLeastFavoriteCustomer(){
+    leastFavouriteCustomer = 'Ken'; 
+}
